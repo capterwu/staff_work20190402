5e4d2708b112cd724cf83fdd23cfa859e8c69c44
RedorBlack <243309672@qq.com>
2019-04-02 15:17:01 +0800

    算法

---
+++ src/main/java/com/password/pass/service/PassService.java
@@ -0,0 +1,17 @@
+package com.password.pass.service;
+
+import java.util.List;
+
+/**
+ * 描述:
+ *
+ * @author Red
+ * @create 2019/4/1 17:36
+ */
+public interface PassService {
+
+
+    List<String> getPassList();
+
+
+}
